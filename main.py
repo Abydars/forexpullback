1d382d54abba28687b1302c3d71ce8acfd430751
Abid R. <abidr.w@gmail.com>
2026-04-23 23:24:07 +0500

    Update main.py

--- main.py
+++ main.py
@@ -6,7 +6,10 @@ init_db()
 from app.engine.lifecycle import start_engine, stop_engine
 from app.ui import build_layout
 
-build_layout()
+@ui.page('/')
+def index():
+    build_layout()
+
 app.on_startup(start_engine)
 app.on_shutdown(stop_engine)
 ui.run(title='Forex Pullback Bot', port=8080, reload=False, show=False, dark=True)
